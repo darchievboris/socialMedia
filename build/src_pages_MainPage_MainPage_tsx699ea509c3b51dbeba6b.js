b5ab7abfe1fa941ea658b2fb07a2b6ce4348c7c0
darchievboris <darchievboris@gmail.com>
2023-06-12 10:38:43 +0300

    added Routes and decomposed bundle on chanks whith code-spliting

---
+++ build/src_pages_MainPage_MainPage_tsx699ea509c3b51dbeba6b.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunksocialmedia"] = self["webpackChunksocialmedia"] || []).push([["src_pages_MainPage_MainPage_tsx"],{
+
+/***/ "./src/pages/MainPage/MainPage.tsx":
+/*!*****************************************!*\
+  !*** ./src/pages/MainPage/MainPage.tsx ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var MainPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "MainPage" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MainPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX01haW5QYWdlX01haW5QYWdlX3RzeDY5OWVhNTA5YzNiNTFkYmViYTZiLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHNCQUFzQjtBQUNoRDtBQUNBLGlFQUFlLFFBQVEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3NvY2lhbG1lZGlhLy4vc3JjL3BhZ2VzL01haW5QYWdlL01haW5QYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xudmFyIE1haW5QYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIk1haW5QYWdlXCIgfSkpO1xufTtcbmV4cG9ydCBkZWZhdWx0IE1haW5QYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
